ffc29f903be786e9873dade4a4eab281cd5152f7
David Cuthbert <dacut@kanga.org>
2014-01-02 16:28:46 -0800

    Start of note move support.

--- static/notepage.js
+++ static/notepage.js
@@ -18,10 +18,10 @@ jQuery(function($) {
             domNode = $("#" + domId);
         }
 
-        style = {'width': (note.width_um * pixels_per_um),
-                 'height': (note.height_um * pixels_per_um),
-                 'left': (note.x_pos_um * pixels_per_um),
-                 'top': (note.y_pos_um * pixels_per_um),
+        style = {'width': (0.001 * note.width_um) + "mm",
+                 'height': (0.001 * note.height_um) + "mm",
+                 'left': (0.001 * note.x_pos_um) + "mm",
+                 'top': (0.001 * note.y_pos_um) + "mm",
                  'z-index': note.z_index}
 
         domNode.css(style);
@@ -30,10 +30,62 @@ jQuery(function($) {
 
         // FIXME: Need to render Markdown into HTML.
         domNode.text(note.contents_markdown);
+
+        domNode.mousedown(onNoteMouseDown);
+        domNode.mouseup(onNoteMouseUp);
+        domNode.mousemove(onNoteMouseMove);
+        domNode.mouseleave(onNoteMouseLeave);
+    }
+
+    function onNoteMouseDown(e) {
+        var target = $(e.target);
+        if (e.button === 0) {
+            target.data("dragging", [e.clientX, e.clientY]);
+        }
+    }
+
+    function onNoteMouseUp(e) {
+        var target = $(e.target);
+        if (e.button === 0) {
+            target.removeData("dragging");
+        }
+    }
+
+    function onNoteMouseLeave(e) {
+        var target = $(e.target);
+        target.removeData("dragging");
+    }
+
+    function onNoteMouseMove(e) {
+        var target = $(e.target);
+        var start = target.data("dragging");
+        var curLeft, curTop, newLeft, newTop;
+
+        if (start) {
+            curLeft = target.css('left');
+            curTop = target.css('top');
+            curLeft = Number(curLeft.substring(0, curLeft.length - 2));
+            curTop = Number(curTop.substring(0, curTop.length - 2));
+            var newLeft = curLeft + e.clientX - start[0];
+            var newTop = curTop + e.clientY - start[1];
+
+            console.log("dragging: clientX=" + e.clientX + ", clientY=" + e.clientY + ", target.left=" + target.css('left') + ", target.top=" + target.css('top') + ", start=" + start[0] + "," + start[1] + ", target.position=" + target.css('position') + ", new=" + newLeft + "," + newTop);
+
+            target.css("left", newLeft);
+            target.css("top", newTop);
+            target.data("dragging", [e.clientX, e.clientY]);
+        }
     }
 
     $("#createNoteAction").click(function () {
         console.log("create note clicked");
         dozer.create_note(node.full_name, onCreateNoteSuccess, null);
     });
+
+    (function () {
+        for (var i = 0; i < node_contents.length; ++i) {
+            var note = node_contents[i];
+            drawNote(note);
+        }
+    })();
 });
