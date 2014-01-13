93e9bd79355009388daff595245e7b1554f83bf4
David Cuthbert <dacut@kanga.org>
2014-01-12 17:06:30 -0800

    Add interstital canvas.

--- static/notepage.js
+++ static/notepage.js
@@ -1,6 +1,11 @@
 jQuery(function($) {
     var viewport = $("#viewport");
+    var canvas = $("#canvas");
     var edited_note = null;
+    var dragNoteDOM = null;
+    var dragNote = null;
+    var dragLastX = 0;
+    var dragLastY = 0;
 
     function onCreateNoteSuccess(id, note) {
         window.notepage.children.push(note);
@@ -11,8 +16,6 @@ jQuery(function($) {
         var notepage_revision_id = result_block['notepage_revision_id'];
         var results = result_block['results'];
         var i, result, note, pos_um, size_um, z_index, contents_markdown;
-
-        console.log("Successful update");
 
         window.notepage.revision_id = notepage_revision_id;
         
@@ -66,7 +69,7 @@ jQuery(function($) {
             // New note; need to create it.
             $('<div class="note" id="' + domId + '">' +
               '<div class="note-contents"></div>' +
-              '</div>').appendTo(viewport);
+              '</div>').appendTo(canvas);
             noteDOM = $("#" + domId);
             noteDOM.data("note", note);
             noteContentsDOM = $(".note-contents", noteDOM);
@@ -106,29 +109,24 @@ jQuery(function($) {
     }
 
     function stopDragging() {
-        // Stop any drag operation currently in progress.
-        var targetId = viewport.data("drag-target");
-        var target, note, pos_um;
+        var note = dragNote;
+        var pos_um;
 
-        if (targetId === undefined) {
+        // Stop any drag operation currently in progress.
+        if (! dragNoteDOM) {
             // Nothing being dragged.
-            return;
+            return false;
         }
 
-        target = $("#" + targetId);
-        note = target.data('note');
-        pos_um = getNotePositionInMicrons(target);
-        viewport.removeData("drag-target");
-        viewport.removeData("drag-last-x");
-        viewport.removeData("drag-last-y");
+        pos_um = getNotePositionInMicrons(dragNoteDOM);
 
-        console.log("stopDragging: pos_um=" + JSON.stringify(pos_um) +
-                    ", note.pos_um=" + JSON.stringify(note.pos_um));
+        // Stop the drag action
+        dragNoteDOM = null;
+        dragNote = null;
 
-        // Did we actually move the note?
         if (pos_um[0] === note.pos_um[0] && pos_um[1] === note.pos_um[1]) {
-            console.log("Skipping update; no change in position.");
-            return;
+            // The note didn't move; don't fire an update event.
+            return false;
         }
 
         // Update the server with the new note position.
@@ -140,6 +138,8 @@ jQuery(function($) {
               "pos_um": pos_um}],
             onUpdateNotepageSuccess,
             onUpdateNotepageError);
+        
+        return false;
     }
 
     function onNoteMouseDown(e) {
@@ -152,14 +152,56 @@ jQuery(function($) {
         }
 
         if (e.button === 0 && e.target === e.currentTarget) {
-            viewport.data("drag-target", noteDOM.attr("id"));
-            viewport.data("drag-last-x", e.clientX);
-            viewport.data("drag-last-y", e.clientY);
+            // Left button being pressed on the note itself.
+            dragNoteDOM = noteDOM;
+            dragNote = noteDOM.data("note");
+            dragLastX = e.clientX;
+            dragLastY = e.clientY;
 
             // Don't let this event bubble.
             return false;
         }
     }
+
+    function onMouseMove(e) {
+        // Handle a mouse motion event on the canvas.  If a drag event is in
+        // progress, this updates the position of the note on the canvas.
+        var deltaX, deltaY, left, top;
+
+        if (dragNoteDOM) {
+            // A note is being moved.
+
+            if (e.which !== 1) {
+                // Mouse button was released, but we didn't receive the
+                // mouseup event.  Go ahead and release the drag now.
+                stopDragging();
+                return;
+            }
+
+            // Delta is the difference between the current position in the
+            // client window and the last reported position.
+            deltaX = e.clientX - dragLastX;
+            deltaY = e.clientY - dragLastY;
+
+            // Get the position of the target.  We use the truncating
+            // "feature" of parseInt to get rid of the "px" suffix and
+            // convert the string to an integer.
+            left = parseInt(dragNoteDOM.css('left'));
+            top = parseInt(dragNoteDOM.css('top'));
+
+            // Modify the position by the delta.
+            left += deltaX;
+            top += deltaY;
+
+            dragNoteDOM.css("left", left);
+            dragNoteDOM.css("top", top);
+
+            // Remember the last reported position.
+            dragLastX = e.clientX;
+            dragLastY = e.clientY;
+        }
+    }
+
 
     function onNoteDoubleClick(e) {
         // Handle a double-click event on a note.  This starts editing the
@@ -173,7 +215,6 @@ jQuery(function($) {
         }
 
         edited_note = noteDOM.data("note");
-        console.log("edited_note=" + JSON.stringify(edited_note));
 
         // Remove the display-only contents while editing.
         $(".note-contents", noteDOM).css("display", "none");
@@ -199,8 +240,6 @@ jQuery(function($) {
     function onNoteEditKeyDown(e) {
         var target = $(e.target);
         var text;
-
-        console.log("e.which == " + e.which);
 
         if (e.which === 27) {
             // Escape -- cancel editing.
@@ -230,50 +269,6 @@ jQuery(function($) {
         return true;
     }
 
-    function onMouseMove(e) {
-        // Handle a mouse motion event on the viewport.  If a drag event is in
-        // progress, this updates the position of the note on the viewport.
-        var targetId = viewport.data("drag-target");
-        var target, lastX, lastY, deltaX, deltaY, left, top;
-
-        if (e.which != 1 && targetId !== undefined && targetId !== null) {
-            // Mouse button was released, but we didn't receive the mouseup
-            // event (pointer outside of window).  Go ahead and release the
-            // drag now.
-            stopDragging();
-            return;
-        }
-
-        if (targetId !== undefined && targetId !== null) {
-            target = $("#" + targetId);
-
-            lastX = viewport.data("drag-last-x");
-            lastY = viewport.data("drag-last-y");
-
-            // Delta is the difference between the current position in the
-            // client window and the last reported position.
-            deltaX = e.clientX - lastX;
-            deltaY = e.clientY - lastY;
-
-            // Get the position of the target and remove the 'px' suffix.
-            left = target.css('left');
-            top = target.css('top');
-            left = Number(left.substring(0, left.length - 2));
-            top = Number(top.substring(0, top.length - 2));
-            
-            // Modify the position by the delta.
-            left += deltaX;
-            top += deltaY;
-
-            target.css("left", left);
-            target.css("top", top);
-
-            // Remember the last reported position.
-            viewport.data("drag-last-x", e.clientX);
-            viewport.data("drag-last-y", e.clientY);
-        }
-    }
-
     function getPixelsPerMicron() {
         var sizetest = $('<div id="sizetest" style="position: relative; ' +
                          'width: 100mm; height: 100mm; ' +
@@ -285,16 +280,12 @@ jQuery(function($) {
         // rounding errors.
 
         sizetest.appendTo("#viewport")
-        width = $("#sizetest").css("width");
-        height = $("#sizetest").css("height");
-
-        console.log("width=" + width);
-
-        width = Number(width.substring(0, width.length - 2));
-        height = Number(height.substring(0, height.length - 2));
+        sizetest = $("#sizetest");
+        width = parseInt(sizetest.css("width"));
+        height = parseInt(sizetest.css("height"));
 
         // Remove our dummy div.
-        $("#sizetest").remove();
+        sizetest.remove();
 
         // We need to scale by 1e-5 to go from hundreds of mm to microns.
         return [1e-5 * width, 1e-5 * height];
@@ -314,16 +305,13 @@ jQuery(function($) {
     }
 
     function getNoteSizeInMicrons(noteDOM, pixelsPerMicron) {
-        var width = noteDOM.css("width");
-        var height = noteDOM.css("height");
+        var width = parseInt(noteDOM.css("width"));
+        var height = parseInt(noteDOM.css("height"));
 
         if (pixelsPerMicron === undefined || pixelsPerMicron === null) {
             pixelsPerMicron = getPixelsPerMicron();
         }
         
-        width = Number(width.substring(0, width.length - 2));
-        height = Number(height.substring(0, height.length - 2));
-
         width = Math.round(width / pixelsPerMicron[0]);
         height = Math.round(height / pixelsPerMicron[1]);
 
@@ -343,23 +331,17 @@ jQuery(function($) {
             pixelsPerMicron = getPixelsPerMicron();
         }
         
-        left = Number(left.substring(0, left.length - 2));
-        top = Number(top.substring(0, top.length - 2));
-
-        left = Math.round(left / pixelsPerMicron[0]);
-        top = Math.round(top / pixelsPerMicron[1]);
+        left = Math.round(parseInt(left) / pixelsPerMicron[0]);
+        top = Math.round(parseInt(top) / pixelsPerMicron[1]);
         
         return [left, top];
     }
 
     // Resize the viewport so its height is the height of the document.
-    console.log("window.innerHeight=" + window.innerHeight +
-                ", viewport.offset().top=" + viewport.offset().top);
     viewport.height(window.innerHeight - viewport.offset().top - 20);
 
     // Bind the "Create note" link.
     $("#createNoteAction").click(function () {
-        console.log("create note clicked");
         dozer.create_note(window.notepage.node_id, onCreateNoteSuccess, null);
     });
 
