0838dc6dd0e68eda9557c3fa8f8281670dff8206
David Cuthbert <dacut@kanga.org>
2014-01-15 10:30:05 -0800

    Move drag APIs into their own namespace.

--- static/notepage.js
+++ static/notepage.js
@@ -2,272 +2,7 @@ jQuery(function($) {
     var viewport = $("#viewport");
     var canvas = $("#canvas");
     var edited_note = null;
-    var dragNoteDOM = null;
-    var dragNote = null;
-    var dragLastX = 0;
-    var dragLastY = 0;
-
-    function onCreateNoteSuccess(id, note) {
-        window.notepage.children.push(note);
-        drawNote(note);
-    }
-
-    function onUpdateNotepageSuccess(id, result_block) {
-        var notepage_revision_id = result_block['notepage_revision_id'];
-        var results = result_block['results'];
-        var i, result, note, pos_um, size_um, z_index, contents_markdown;
-
-        window.notepage.revision_id = notepage_revision_id;
-        
-        for (i = 0; i < results.length; ++i) {
-            result = results[i];
-            note = getNoteById(result['note_id']);
-            if (note === null) {
-                console.log("onUpdateNotepageSuccess: cannot find note id " +
-                            result['note_id']);
-                continue;
-            }
-
-            pos_um = result['pos_um'];
-            size_um = result['size_um'];
-            z_index = result['z_index'];
-            contents_markdown = result['contents_markdown'];
-
-            if (pos_um !== undefined && pos_um !== null) {
-                note.pos_um = pos_um;
-            }
-
-            if (size_um !== undefined && size_um !== null) {
-                note.size_um = size_um;
-            }
-
-            if (z_index !== undefined && z_index !== null) {
-                note.z_index = z_index;
-            }
-
-            if (contents_markdown !== undefined && contents_markdown !== null) {
-                note.contents_markdown = contents_markdown;
-            }
-
-            drawNote(note);
-        }
-
-        return;
-    }
-
-    function onUpdateNotepageError(id) {
-        console.log("Failed update");
-    }
-
-    function drawNote(note) {
-        var domId, noteDOM, noteContentsDOM, style, converter;
-
-        domId = "note-" + note.node_id;
-        noteDOM = $("#" + domId);
-        
-        if (noteDOM.length == 0) {
-            // New note; need to create it.
-            $('<div class="note" id="' + domId + '">' +
-              '<div class="note-contents"></div>' +
-              '</div>').appendTo(canvas);
-            noteDOM = $("#" + domId);
-            noteDOM.data("note", note);
-            noteContentsDOM = $(".note-contents", noteDOM);
-
-            style = {'width': (0.001 * note.size_um[0]) + "mm",
-                     'height': (0.001 * note.size_um[1]) + "mm",
-                     'left': (0.001 * note.pos_um[0]) + "mm",
-                     'top': (0.001 * note.pos_um[1]) + "mm",
-                     'z-index': note.z_index}
-
-            noteDOM.css(style);
-            noteDOM.mousedown(onNoteMouseDown);
-            noteDOM.dblclick(onNoteDoubleClick);
-            noteContentsDOM.mousedown(onNoteMouseDown);
-        } else {
-            noteContentsDOM = $(".note-contents", noteDOM);
-        }
-
-        // Convert the raw text into HTML.
-        converter = Markdown.getSanitizingConverter();
-        noteContentsDOM.html(
-            converter.makeHtml(note.contents_markdown));
-
-        // Make sure the contents are visible.
-        noteContentsDOM.css("display", "block");
-    }
-
-    function updateNote(note, text) {
-        dozer.update_notepage(
-            window.notepage.node_id,
-            [{"action": "edit_note",
-              "note_id": note.node_id,
-              "revision_id": note.revision_id,
-              "contents_markdown": text}],
-            onUpdateNotepageSuccess,
-            onUpdateNotepageError);
-    }
-
-    function stopDragging() {
-        var note = dragNote;
-        var pos_um;
-
-        // Stop any drag operation currently in progress.
-        if (! dragNoteDOM) {
-            // Nothing being dragged.
-            return false;
-        }
-
-        pos_um = getNotePositionInMicrons(dragNoteDOM);
-
-        // Stop the drag action
-        dragNoteDOM = null;
-        dragNote = null;
-
-        if (pos_um[0] === note.pos_um[0] && pos_um[1] === note.pos_um[1]) {
-            // The note didn't move; don't fire an update event.
-            return false;
-        }
-
-        // Update the server with the new note position.
-        dozer.update_notepage(
-            window.notepage.node_id,
-            [{"action": "edit_note",
-              "note_id": note.node_id,
-              "revision_id": note.revision_id,
-              "pos_um": pos_um}],
-            onUpdateNotepageSuccess,
-            onUpdateNotepageError);
-        
-        return false;
-    }
-
-    function onNoteMouseDown(e) {
-        // Handle a mousedown event on a note.  This starts dragging the note.
-        var noteDOM = $(e.target);
-
-        if (! noteDOM.is(".note")) {
-            // Mouse down on an inner item; select the note div.
-            noteDOM = noteDOM.parents(".note");
-        }
-
-        if (e.button === 0 && e.target === e.currentTarget) {
-            // Left button being pressed on the note itself.
-            dragNoteDOM = noteDOM;
-            dragNote = noteDOM.data("note");
-            dragLastX = e.clientX;
-            dragLastY = e.clientY;
-
-            // Don't let this event bubble.
-            return false;
-        }
-    }
-
-    function onMouseMove(e) {
-        // Handle a mouse motion event on the canvas.  If a drag event is in
-        // progress, this updates the position of the note on the canvas.
-        var deltaX, deltaY, left, top;
-
-        if (dragNoteDOM) {
-            // A note is being moved.
-
-            if (e.which !== 1) {
-                // Mouse button was released, but we didn't receive the
-                // mouseup event.  Go ahead and release the drag now.
-                stopDragging();
-                return;
-            }
-
-            // Delta is the difference between the current position in the
-            // client window and the last reported position.
-            deltaX = e.clientX - dragLastX;
-            deltaY = e.clientY - dragLastY;
-
-            // Get the position of the target.  We use the truncating
-            // "feature" of parseInt to get rid of the "px" suffix and
-            // convert the string to an integer.
-            left = parseInt(dragNoteDOM.css('left'));
-            top = parseInt(dragNoteDOM.css('top'));
-
-            // Modify the position by the delta.
-            left += deltaX;
-            top += deltaY;
-
-            dragNoteDOM.css("left", left);
-            dragNoteDOM.css("top", top);
-
-            // Remember the last reported position.
-            dragLastX = e.clientX;
-            dragLastY = e.clientY;
-        }
-    }
-
-
-    function onNoteDoubleClick(e) {
-        // Handle a double-click event on a note.  This starts editing the
-        // note.
-        var noteDOM = $(e.target), form, editor;
-
-        if (! noteDOM.is(".note")) {
-            // Clicked on the contents div; set noteDOM to the enclosing
-            // note div.
-            noteDOM = noteDOM.parents(".note");
-        }
-
-        edited_note = noteDOM.data("note");
-
-        // Remove the display-only contents while editing.
-        $(".note-contents", noteDOM).css("display", "none");
-
-        // Create an edit form in the interior.
-        form = $('<form><textarea id="note-edit" cols="132" rows="60">' +
-                 '</textarea></form>');
-        form.appendTo(noteDOM);
-        editor = $("#note-edit", form);
-
-        // Populate the value of the note with the contents.
-        editor.text(edited_note.contents_markdown);
-
-        // Watch keypresses for Alt+Enter or Escape, which terminate the
-        // editing process.
-        editor.keydown(onNoteEditKeyDown);
-        editor.trigger("focus");
-
-        // Don't let this event bubble.
-        return false;
-    }
-
-    function onNoteEditKeyDown(e) {
-        var target = $(e.target);
-        var text;
-
-        if (e.which === 27) {
-            // Escape -- cancel editing.
-            target.parent().remove();
-            drawNote(edited_note);
-            edited_note = null;
-            return false;
-        }
-
-        if ((e.altKey || e.metaKey) && (e.which === 10 || e.which === 13)) {
-            // Alt+Enter pressed -- save changes.
-            
-            // Get the text entered.
-            text = target.val();
-
-            // Remove the form.
-            target.parent().remove();
-
-            // Update the note.
-            updateNote(edited_note, text);
-
-            edited_note = false;
-            return false;
-        }
-
-        // Normal key; ignore it, let it bubble up.
-        return true;
-    }
+    var drag;
 
     function getPixelsPerMicron() {
         var sizetest = $('<div id="sizetest" style="position: relative; ' +
@@ -337,6 +72,292 @@ jQuery(function($) {
         return [left, top];
     }
 
+    function drawNote(note) {
+        var domId, noteDOM, noteContentsDOM, style, converter;
+
+        domId = "note-" + note.node_id;
+        noteDOM = $("#" + domId);
+        
+        if (noteDOM.length == 0) {
+            // New note; need to create it.
+            $('<div class="note" id="' + domId + '">' +
+              '<div class="note-contents"></div>' +
+              '</div>').appendTo(canvas);
+            noteDOM = $("#" + domId);
+            noteDOM.data("note", note);
+            noteContentsDOM = $(".note-contents", noteDOM);
+
+            style = {'width': (0.001 * note.size_um[0]) + "mm",
+                     'height': (0.001 * note.size_um[1]) + "mm",
+                     'left': (0.001 * note.pos_um[0]) + "mm",
+                     'top': (0.001 * note.pos_um[1]) + "mm",
+                     'z-index': note.z_index}
+
+            noteDOM.css(style);
+            noteDOM.mousedown(onNoteMouseDown);
+            noteDOM.dblclick(onNoteDoubleClick);
+            noteContentsDOM.mousedown(onNoteMouseDown);
+        } else {
+            noteContentsDOM = $(".note-contents", noteDOM);
+        }
+
+        // Convert the raw text into HTML.
+        converter = Markdown.getSanitizingConverter();
+        noteContentsDOM.html(
+            converter.makeHtml(note.contents_markdown));
+
+        // Make sure the contents are visible.
+        noteContentsDOM.css("display", "block");
+    }
+
+    function updateNote(note, text) {
+        dozer.update_notepage(
+            window.notepage.node_id,
+            [{"action": "edit_note",
+              "note_id": note.node_id,
+              "revision_id": note.revision_id,
+              "contents_markdown": text}],
+            onUpdateNotepageSuccess,
+            onUpdateNotepageError);
+    }
+
+    drag = (function () {
+        var dragNoteDOM = null;
+        var dragNote = null;
+        var dragLastX = 0;
+        var dragLastY = 0;
+        var start, stop, move;
+
+        stop = function () {
+            var note = dragNote;
+            var pos_um;
+
+            // Stop any drag operation currently in progress.
+            if (! dragNoteDOM) {
+                // Nothing being dragged.
+                return false;
+            }
+
+            pos_um = getNotePositionInMicrons(dragNoteDOM);
+
+            // Stop the drag action
+            dragNoteDOM = null;
+            dragNote = null;
+
+            if (pos_um[0] === note.pos_um[0] &&
+                pos_um[1] === note.pos_um[1]) {
+                // The note didn't move; don't fire an update event.
+                return false;
+            }
+
+            // Update the server with the new note position.
+            dozer.update_notepage(
+                window.notepage.node_id,
+                [{"action": "edit_note",
+                  "note_id": note.node_id,
+                  "revision_id": note.revision_id,
+                  "pos_um": pos_um}],
+                onUpdateNotepageSuccess,
+                onUpdateNotepageError);
+        
+            return false;
+        };
+
+        start = function (e) {
+            // Find the note being dragged.
+            dragNoteDOM = $(e.target);
+
+            if (! dragNoteDOM.is(".note")) {
+                // The button was pressed on an element within the note.
+                dragNoteDOM = dragNoteDOM.parents(".note");
+            }
+
+            dragNote = dragNoteDOM.data("note");
+            dragLastX = e.clientX;
+            dragLastY = e.clientY;
+            return false;
+        };
+
+        move = function (e) {
+            var deltaX, deltaY, left, top;
+
+            if (! dragNoteDOM) {
+                // Nothing is being dragged.
+                return false;
+            }
+
+            if (e.which !== 1) {
+                // Mouse button was released; stop dragging.
+                return stop(e);
+            }
+
+            // Delta is the difference between the current position in the
+            // client window and the last reported position.
+            deltaX = e.clientX - dragLastX;
+            deltaY = e.clientY - dragLastY;
+
+            // Get the position of the target.  We use the truncating
+            // "feature" of parseInt to get rid of the "px" suffix and
+            // convert the string to an integer.
+            left = parseInt(dragNoteDOM.css('left'));
+            top = parseInt(dragNoteDOM.css('top'));
+
+            // Modify the position by the delta.
+            left += deltaX;
+            top += deltaY;
+
+            dragNoteDOM.css("left", left);
+            dragNoteDOM.css("top", top);
+
+            // Remember the last reported position.
+            dragLastX = e.clientX;
+            dragLastY = e.clientY;
+        };
+
+        return {'start': start, 'stop': stop, 'move': move};
+    })();
+
+    function onCreateNoteSuccess(id, note) {
+        notepage.children.push(note);
+        drawNote(note);
+    }
+
+    function onUpdateNotepageSuccess(id, result_block) {
+        var notepage_revision_id = result_block['notepage_revision_id'];
+        var results = result_block['results'];
+        var i, result, note, pos_um, size_um, z_index, contents_markdown;
+
+        window.notepage.revision_id = notepage_revision_id;
+        
+        for (i = 0; i < results.length; ++i) {
+            result = results[i];
+            note = getNoteById(result['note_id']);
+            if (note === null) {
+                console.log("onUpdateNotepageSuccess: cannot find note id " +
+                            result['note_id']);
+                continue;
+            }
+
+            pos_um = result['pos_um'];
+            size_um = result['size_um'];
+            z_index = result['z_index'];
+            contents_markdown = result['contents_markdown'];
+
+            if (pos_um !== undefined && pos_um !== null) {
+                note.pos_um = pos_um;
+            }
+
+            if (size_um !== undefined && size_um !== null) {
+                note.size_um = size_um;
+            }
+
+            if (z_index !== undefined && z_index !== null) {
+                note.z_index = z_index;
+            }
+
+            if (contents_markdown !== undefined && contents_markdown !== null) {
+                note.contents_markdown = contents_markdown;
+            }
+
+            drawNote(note);
+        }
+
+        return;
+    }
+
+    function onUpdateNotepageError(id) {
+        console.log("Failed update");
+    }
+
+    
+    function onNoteMouseDown(e) {
+        if (e.button === 0 && e.target === e.currentTarget) {
+            // Left button being pressed on the note itself.
+            return drag.start(e);
+        }
+    }
+
+    function onWindowMouseMove(e) {
+        // Handle mouse motion events.  If a drag event is in progress, this
+        // updates the position of the note on the canvas.
+        return drag.move(e);
+    }
+
+    function onWindowMouseUp(e) {
+        // If a drag event is in progress, stop it.
+        return drag.stop(e);
+    }
+
+    function onWindowBlur(e) {
+        // If a drag event is in progress, stop it.
+        return drag.stop(e);
+    }
+
+    function onNoteDoubleClick(e) {
+        // Handle a double-click event on a note.  This starts editing the
+        // note.
+        var noteDOM = $(e.target), form, editor;
+
+        if (! noteDOM.is(".note")) {
+            // Clicked on the contents div; set noteDOM to the enclosing
+            // note div.
+            noteDOM = noteDOM.parents(".note");
+        }
+
+        edited_note = noteDOM.data("note");
+
+        // Remove the display-only contents while editing.
+        $(".note-contents", noteDOM).css("display", "none");
+
+        // Create an edit form in the interior.
+        form = $('<form><textarea id="note-edit" cols="132" rows="60">' +
+                 '</textarea></form>');
+        form.appendTo(noteDOM);
+        editor = $("#note-edit", form);
+
+        // Populate the value of the note with the contents.
+        editor.text(edited_note.contents_markdown);
+
+        // Watch keypresses for Alt+Enter or Escape, which terminate the
+        // editing process.
+        editor.keydown(onNoteEditKeyDown);
+        editor.trigger("focus");
+
+        // Don't let this event bubble.
+        return false;
+    }
+
+    function onNoteEditKeyDown(e) {
+        var target = $(e.target);
+        var text;
+
+        if (e.which === 27) {
+            // Escape -- cancel editing.
+            target.parent().remove();
+            drawNote(edited_note);
+            edited_note = null;
+            return false;
+        }
+
+        if ((e.altKey || e.metaKey) && (e.which === 10 || e.which === 13)) {
+            // Alt+Enter pressed -- save changes.
+            
+            // Get the text entered.
+            text = target.val();
+
+            // Remove the form.
+            target.parent().remove();
+
+            // Update the note.
+            updateNote(edited_note, text);
+
+            edited_note = false;
+            return false;
+        }
+
+        // Normal key; ignore it, let it bubble up.
+        return true;
+    }
     // Resize the viewport so its height is the height of the document.
     viewport.height(window.innerHeight - viewport.offset().top - 20);
 
@@ -346,9 +367,9 @@ jQuery(function($) {
     });
 
     // Bind motion events to allow dragging of notes.
-    $(window).mousemove(onMouseMove);
-    $(window).mouseup(stopDragging);
-    $(window).on("blur", stopDragging);
+    $(window).mousemove(onWindowMouseMove);
+    $(window).mouseup(onWindowMouseUp);
+    $(window).on("blur", onWindowBlur);
 
     // Add each note to the document.
     (function () {
